5c31e94de79f6378e0d0a428353550b6237e6517
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2024-02-14 22:11:12 +0000

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    with open('chatapp/website/config.json', 'r') as config_file:
+    with open('chatapp/config.json', 'r') as config_file:
             config = json.load(config_file)
     app.config['SECRET_KEY'] = config['SECRET_KEY']
     app.config['UPLOAD_FOLDER'] = config['UPLOAD_FOLDER']
